9bdeeba5f901102e5c312621f01aa4e8bc189c26
mateuseduardomedeiros <eduardomedeirosrn2@gmail.com>
2020-12-09 00:08:08 -0300

    Final | Adição de proteção nas rotas de usuário

--- backend/controllers/UsuarioController.ts
+++ backend/controllers/UsuarioController.ts
@@ -1,17 +1,25 @@
 import { Usuario } from "../models/Usuario";
 import { AbstractController } from "./AbstractController";
-
+const auth = require("../middlewares/auth");
 export class UsuarioController extends AbstractController {
   protected prefix: string = "/usuario";
 
   get() {
     return async (req: any, res: any, next: any) => {
+      let autenticou = await auth(req, res);
+      if (autenticou.error) {
+        return res.status(403).json({ msg: autenticou.msg });
+      }
       return res.status(200).json(await Usuario.find());
     };
   }
 
   show() {
     return async (req: any, res: any, next: any) => {
+      let autenticou = await auth(req, res);
+      if (autenticou.error) {
+        return res.status(403).json({ msg: autenticou.msg });
+      }
       try {
         let usuario: Usuario | undefined = await Usuario.findOne({
           id: req.params.id,
